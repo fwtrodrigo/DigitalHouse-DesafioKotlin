169297f39164e9716ecbe389c984dcab998ca0dc
Rodrigo Freitas <fwtcursos@gmail.com>
2020-09-25 23:48:02 -0300

    Adicao classe Curso + Atualizacao UML

---
+++ src/br/com/digitalhouse/desafio/Curso.kt
@@ -0,0 +1,21 @@
+package br.com.digitalhouse.desafio
+
+class Curso(val codigo: Int, var nome: String) {
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        return other is Curso && other.codigo == codigo
+    }
+
+    override fun hashCode(): Int {
+        return codigo
+    }
+
+    override fun toString(): String {
+        return "Curso (codigo=$codigo, nome='$nome')"
+    }
+
+
+}
