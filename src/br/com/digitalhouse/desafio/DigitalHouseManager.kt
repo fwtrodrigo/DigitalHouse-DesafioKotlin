b34b91e070d611d58d706d19c976e1b3ae2e3b96
Rodrigo Freitas <fwtcursos@gmail.com>
2020-10-02 02:51:53 -0300

    Implementacao classe DigitalHouseManager

---
+++ src/br/com/digitalhouse/desafio/DigitalHouseManager.kt
@@ -0,0 +1,8 @@
+package br.com.digitalhouse.desafio
+
+class DigitalHouseManager {
+    val alunos = mutableSetOf<Aluno>()
+    val professores = mutableSetOf<Professor>()
+    val cursos = mutableSetOf<Curso>()
+    val matriculas = mutableSetOf<Matricula>()
+}
